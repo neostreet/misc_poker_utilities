7e461a747c2afe347b09b0f7fa1b32c0aa31aa0f
Aidan Lloyd <lloyd.aidan@gmail.com>
2012-01-06 21:44:56 -0500

    add all_in_pct

---
+++ all_in_pct.c
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <string.h>
+
+#define FALSE 0
+#define TRUE  1
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: all_in_pct (-debug) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int Contains(int bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+int all_in(char *line,int line_len,int line_no);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  int bDebug;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int file_no;
+  int all_ins;
+  double all_in_pct;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = FALSE;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = TRUE;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  file_no = 0;
+  all_ins = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    file_no++;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    line_no = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      line_no++;
+
+      if (all_in(line,line_len,line_no))
+        all_ins++;
+    }
+
+    fclose(fptr);
+  }
+
+  all_in_pct = (double)all_ins / (double)file_no;
+
+  if (!bDebug)
+    printf("%lf\n",all_in_pct);
+  else {
+    printf("%lf (%d of %d)\n",all_in_pct,all_ins,file_no);
+  }
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int Contains(int bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return FALSE;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return TRUE;
+    }
+  }
+
+  return FALSE;
+}
+
+int all_in(char *line,int line_len,int line_no)
+{
+  int ix;
+
+  if (Contains(TRUE,
+    line,line_len,
+    "neostreet",9,
+    &ix)) {
+
+    if (Contains(TRUE,
+      line,line_len,
+      "all-in",6,
+      &ix)) {
+
+      return 1;
+    }
+  }
+
+  return 0;
+}
