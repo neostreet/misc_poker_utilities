04a15ef7a08f9811cfd7d7c9de25a2ee9d5bb7fe
Aidan Lloyd <lloyd.aidan@gmail.com>
2012-05-06 11:27:11 -0400

    add rockets

---
+++ rockets.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <string.h>
+
+#define FALSE 0
+#define TRUE  1
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: rockets (-debug) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  int bDebug;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int rocket_count;
+  double rocket_pct;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = FALSE;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = TRUE;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  line_no = 0;
+  rocket_count = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    if ((line_len >= 5) && (line[0] == 'A') && (line[3] == 'A')) {
+      rocket_count++;
+
+      if (bDebug)
+        printf("%s\n",line);
+    }
+  }
+
+  if (bDebug && (rocket_count > 0))
+    putchar(0x0a);
+
+  rocket_pct = (double)rocket_count * (double)100 / (double)line_no;
+
+  printf("%10d of %10d hands (%6.3lf%%)\n",rocket_count,line_no,rocket_pct);
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
