eaa32db3a791dbf58ac155be056165c9d2482be7
Aidan Lloyd <lloyd.aidan@gmail.com>
2012-05-06 22:03:34 -0400

    add -deltaoffset flag to pair

--- pair.c
+++ pair.c
@@ -8,7 +8,7 @@
 static char line[MAX_LINE_LEN];
 
 static char usage[] =
-"usage: pair (-debug) (-offsetoffset) denomination filename\n";
+"usage: pair (-debug) (-offsetoffset) (-deltaoffsetdeltaoffset denom filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
@@ -18,25 +18,35 @@ int main(int argc,char **argv)
   int curr_arg;
   int bDebug;
   int offset;
+  int bDelta;
+  int delta_offset;
   FILE *fptr;
   int line_len;
   int line_no;
   int pair_count;
   double pair_pct;
+  int delta;
+  int sum_delta;
 
-  if ((argc < 3) || (argc > 5)) {
+  if ((argc < 3) || (argc > 6)) {
     printf(usage);
     return 1;
   }
 
   bDebug = FALSE;
   offset = 0;
+  bDelta = FALSE;
+
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-debug"))
       bDebug = TRUE;
     else if (!strncmp(argv[curr_arg],"-offset",7))
       sscanf(&argv[curr_arg][7],"%d",&offset);
+    else if (!strncmp(argv[curr_arg],"-deltaoffset",12)) {
+      bDelta = TRUE;
+      sscanf(&argv[curr_arg][12],"%d",&delta_offset);
+    }
     else
       break;
   }
@@ -54,6 +64,9 @@ int main(int argc,char **argv)
   line_no = 0;
   pair_count = 0;
 
+  if (bDelta)
+    sum_delta = 0;
+
   for ( ; ; ) {
     GetLine(fptr,line,&line_len,MAX_LINE_LEN);
 
@@ -69,6 +82,13 @@ int main(int argc,char **argv)
 
       if (bDebug)
         printf("%s\n",line);
+
+      if (bDelta) {
+        line[delta_offset + 10] = 0;
+
+        sscanf(&line[delta_offset],"%d",&delta);
+        sum_delta += delta;
+      }
     }
   }
 
@@ -76,6 +96,9 @@ int main(int argc,char **argv)
     putchar(0x0a);
 
   pair_pct = (double)pair_count * (double)100 / (double)line_no;
+
+  if (bDelta)
+    printf("%10d\n\n",sum_delta);
 
   printf("%d of %d hands (%6.3lf%%)\n",pair_count,line_no,pair_pct);
 
