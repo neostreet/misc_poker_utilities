789fa5f9604019e7acf4c884a1feb69b2169d30d
Aidan Lloyd <lloyd.aidan@gmail.com>
2012-05-26 17:45:07 -0400

    use format_date()

--- max_gain.c
+++ max_gain.c
@@ -38,6 +38,12 @@ static struct digit_range date_checks[3] = {
   1, 31     /* day */
 };
 
+static char *months[] = {
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+};
+#define NUM_MONTHS (sizeof months / sizeof (char *))
+
 struct session_info_struct {
   int starting_amount;
   int starting_ix;
@@ -57,6 +63,7 @@ static int get_session_info(
   struct session_info_struct *session_info);
 static time_t cvt_date(char *date_str);
 int elem_compare(const void *elem1,const void *elem2);
+static char *format_date(char *cpt);
 
 int main(int argc,char **argv)
 {
@@ -178,8 +185,7 @@ int main(int argc,char **argv)
           session_info[n].starting_ix);
 
         cpt = ctime(&session_info[n].gain_start_date);
-        cpt[strlen(cpt) - 1] = 0;
-        printf("%s\n",cpt);
+        printf("%s\n",format_date(cpt));
 
         printf(fmt1,
           session_info[n].starting_amount +
@@ -188,8 +194,7 @@ int main(int argc,char **argv)
             session_info[n].num_gain_sessions - 1);
 
         cpt = ctime(&session_info[n].gain_end_date);
-        cpt[strlen(cpt) - 1] = 0;
-        printf("%s\n",cpt);
+        printf("%s\n",format_date(cpt));
 
         printf(fmt2,
           session_info[n].gain_amount,
@@ -222,8 +227,7 @@ int main(int argc,char **argv)
       session_info[sort_ixs[n]].starting_ix);
 
     cpt = ctime(&session_info[sort_ixs[n]].gain_start_date);
-    cpt[strlen(cpt) - 1] = 0;
-    printf("%s\n",cpt);
+    printf("%s\n",format_date(cpt));
 
     printf(fmt1,
       session_info[sort_ixs[n]].starting_amount +
@@ -232,8 +236,7 @@ int main(int argc,char **argv)
         session_info[sort_ixs[n]].num_gain_sessions - 1);
 
     cpt = ctime(&session_info[sort_ixs[n]].gain_end_date);
-    cpt[strlen(cpt) - 1] = 0;
-    printf("%s\n",cpt);
+    printf("%s\n",format_date(cpt));
 
     printf(fmt2,
       session_info[sort_ixs[n]].gain_amount,
@@ -405,4 +408,26 @@ int elem_compare(const void *elem1,const void *elem2)
     return session_info[ix2].gain_start_date -
       session_info[ix1].gain_start_date;
   }
+}
+
+static char *format_date(char *cpt)
+{
+  int month;
+  static char date_buf[11];
+
+  cpt[7] = 0;
+  cpt[10] = 0;
+  cpt[24] = 0;
+
+  for (month = 0; month < NUM_MONTHS; month++) {
+    if (!strcmp(&cpt[4],months[month]))
+      break;
+  }
+
+  if (month == NUM_MONTHS)
+    month = 0;
+
+  sprintf(date_buf,"%s-%02d-%s",&cpt[20],month+1,&cpt[8]);
+
+  return date_buf;
 }
