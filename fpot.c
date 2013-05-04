f57f33351b08bd4d25f5ff31d9c2732daddf4365
unknown <lloyd.aidan@gmail.com>
2013-05-04 15:06:54 -0400

    add fpot.c

---
+++ fpot.c
@@ -0,0 +1,260 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: fpot (-verbose) (-debug) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static char summary[] = "*** SUMMARY ***";
+#define SUMMARY_LEN (sizeof (summary) - 1)
+static char won[] = " won ";
+#define WON_LEN (sizeof (won) - 1)
+static char collected[] = " collected ";
+#define COLLECTED_LEN (sizeof (collected) - 1)
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+int get_work_amount(char *line,int line_len);
+
+int main(int argc,char **argv)
+{
+  int m;
+  int n;
+  int p;
+  int curr_arg;
+  bool bVerbose;
+  bool bDebug;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int dbg_line_no;
+  int ix;
+  int file_no;
+  int dbg_file_no;
+  int num_hands;
+  int dbg;
+  int work;
+  int maxval;
+
+  if ((argc < 2) || (argc > 4)) {
+    printf(usage);
+    return 1;
+  }
+
+  bVerbose = false;
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose")) {
+      bVerbose = true;
+      getcwd(save_dir,_MAX_PATH);
+    }
+    else if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  file_no = 0;
+  dbg_file_no = -1;
+  num_hands = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    file_no++;
+
+    if (dbg_file_no == file_no)
+      dbg = 1;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    num_hands++;
+
+    line_no = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      line_no++;
+
+      if (line_no == dbg_line_no)
+        dbg = 1;
+
+      if (bDebug)
+        printf("line %d %s\n",line_no,line);
+
+      if (!strncmp(line,summary,SUMMARY_LEN)) {
+        if (bDebug)
+          printf("line %d SUMMARY line detected; breaking\n",line_no);
+
+        maxval = 0;
+
+        for ( ; ; ) {
+          GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+          if (feof(fptr))
+            break;
+
+          if (Contains(true,
+            line,line_len,
+            won,WON_LEN,
+            &ix)) {
+            work = get_work_amount(line,line_len);
+
+            if (work > maxval)
+              maxval = work;
+          }
+          else if (Contains(true,
+            line,line_len,
+            collected,COLLECTED_LEN,
+            &ix)) {
+            work = get_work_amount(line,line_len);
+
+            if (work > maxval)
+              maxval = work;
+          }
+        }
+
+        if (!bVerbose)
+          printf("%d\n",maxval);
+        else
+          printf("%10d %s\\%s\n",maxval,save_dir,filename);
+
+        break;
+      }
+    }
+
+    fclose(fptr);
+  }
+
+  fclose(fptr0);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
+
+int get_work_amount(char *line,int line_len)
+{
+  int ix;
+  int chara;
+  int work_amount;
+
+  work_amount = 0;
+
+  for (ix = line_len - 1; (ix >= 0); ix--) {
+    chara = line[ix];
+
+    if ((chara >= '0') && (chara <= '9'))
+      break;
+  }
+
+  if (ix + 1 != line_len);
+    line[ix + 1] = 0;
+
+  for ( ; (ix >= 0); ix--) {
+    chara = line[ix];
+
+    if ((chara < '0') || (chara > '9'))
+      break;
+  }
+
+  sscanf(&line[ix+1],"%d",&work_amount);
+
+  return work_amount;
+}
