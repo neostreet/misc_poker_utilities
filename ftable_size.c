effa13de028631c6448754cf6d515536b580b96f
unknown <lloyd.aidan@gmail.com>
2013-07-09 22:48:43 -0400

    add ftable_size.c and ftable_size3.c

---
+++ ftable_size.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: ftable_size (-terse) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bTerse;
+  FILE *fptr0;
+  int filename_len;
+  int num_files;
+  FILE *fptr;
+  int line_len;
+  int ix;
+  int table_size;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bTerse = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-terse"))
+      bTerse = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  num_files = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    num_files++;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      if (!strncmp(line,"Table '",7)) {
+        if (Contains(true,
+          line,line_len,
+          "-max",4,
+          &ix)) {
+
+          line[ix] = 0;
+          sscanf(&line[ix-1],"%d",&table_size);
+        }
+        else
+          table_size = 0;
+
+        break;
+      }
+    }
+
+    fclose(fptr);
+
+    if (bTerse)
+      printf("%d\n",table_size);
+    else
+      printf("%d %s\n",table_size,filename);
+  }
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
