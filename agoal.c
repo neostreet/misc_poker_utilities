c3f49d38c97e9d51be5b655729ff37b954743923
unknown <lloyd.aidan@gmail.com>
2013-07-30 10:55:47 -0400

    add -mark_winning_session and -mark_losing_session flags

--- agoal.c
+++ agoal.c
@@ -12,7 +12,8 @@ static char save_dir[_MAX_PATH];
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: agoal (-debug) (-verbose) filename\n";
+static char usage[] =
+"usage: agoal (-debug) (-verbose) (-mark_winning_session) (-mark_losing_session) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
@@ -22,6 +23,9 @@ int main(int argc,char **argv)
   int curr_arg;
   bool bDebug;
   bool bVerbose;
+  bool bMarkWinningSession;
+  bool bMarkLosingSession;
+  bool bWinningSession;
   FILE *fptr;
   int work;
   int num_winning_hands;
@@ -34,13 +38,15 @@ int main(int argc,char **argv)
   int line_len;
   int line_no;
 
-  if ((argc < 2) || (argc > 4)) {
+  if ((argc < 2) || (argc > 6)) {
     printf(usage);
     return 1;
   }
 
   bDebug = false;
   bVerbose = false;
+  bMarkWinningSession = false;
+  bMarkLosingSession = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-debug")) {
@@ -49,6 +55,10 @@ int main(int argc,char **argv)
     }
     else if (!strcmp(argv[curr_arg],"-verbose"))
       bVerbose = true;
+    else if (!strcmp(argv[curr_arg],"-mark_winning_session"))
+      bMarkWinningSession = true;
+    else if (!strcmp(argv[curr_arg],"-mark_losing_session"))
+      bMarkLosingSession = true;
     else
       break;
   }
@@ -93,6 +103,13 @@ int main(int argc,char **argv)
     }
   }
 
+  if (bMarkWinningSession || bMarkLosingSession) {
+    if (total_winning_delta > total_losing_delta)
+      bWinningSession = true;
+    else
+      bWinningSession = false;
+  }
+
   if (!num_winning_hands) {
     printf("no winning hands\n");
     return 4;
@@ -109,10 +126,22 @@ int main(int argc,char **argv)
   agoal = avg_gain / avg_loss;
 
   if (!bVerbose) {
-    if (!bDebug)
-      printf("%lf\n",agoal);
-    else
-      printf("%s %lf\n",save_dir,agoal);
+    if (!bDebug) {
+      if (bMarkWinningSession && bWinningSession)
+        printf("%lf (w)\n",agoal);
+      else if (bMarkLosingSession && !bWinningSession)
+        printf("%lf (l)\n",agoal);
+      else
+        printf("%lf\n",agoal);
+    }
+    else {
+      if (bMarkWinningSession && bWinningSession)
+        printf("%s %lf (w)\n",save_dir,agoal);
+      else if (bMarkLosingSession && !bWinningSession)
+        printf("%s %lf (l)\n",save_dir,agoal);
+      else
+        printf("%s %lf\n",save_dir,agoal);
+    }
   }
   else {
     printf("   %7d total_winning_delta\n",total_winning_delta);
@@ -123,7 +152,12 @@ int main(int argc,char **argv)
     printf("   %7d num_losing_hands\n",num_losing_hands);
     printf("%10.2lf avg_loss\n\n",avg_loss);
 
-    printf("%10.2lf agoal\n\n",agoal);
+    if (bMarkWinningSession && bWinningSession)
+      printf("%10.2lf agoal (w)\n\n",agoal);
+    else if (bMarkLosingSession && !bWinningSession)
+      printf("%10.2lf agoal (l)\n\n",agoal);
+    else
+      printf("%10.2lf agoal\n\n",agoal);
   }
 
   fclose(fptr);
