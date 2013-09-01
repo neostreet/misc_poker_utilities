96b18a153030a04b1be7a94c182497ec6bc0a130
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-09-01 10:32:46 -0400

    add -in_sessions flag

--- blue_distance2.c
+++ blue_distance2.c
@@ -3,14 +3,16 @@
 
 #define MAX_STR_LEN 256
 
-static char usage[] = "usage: blue_distance2 (-terse) (-no_dates) filename\n";
+static char usage[] =
+"usage: blue_distance2 (-terse) (-no_dates) (-in_sessions) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 int main(int argc,char **argv)
 {
   int curr_arg;
-  int bTerse;
-  int bNoDates;
+  bool bTerse;
+  bool bNoDates;
+  bool bInSessions;
   FILE *fptr;
   int line_len;
   int line_no;
@@ -18,20 +20,24 @@ int main(int argc,char **argv)
   int delta;
   int balance;
   int max_balance;
+  int max_balance_ix;
 
-  if ((argc < 2) || (argc > 4)) {
+  if ((argc < 2) || (argc > 5)) {
     printf(usage);
     return 1;
   }
 
   bTerse = false;
   bNoDates = false;
+  bInSessions = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-terse"))
       bTerse = true;
     else if (!strcmp(argv[curr_arg],"-no_dates"))
       bNoDates = true;
+    else if (!strcmp(argv[curr_arg],"-in_sessions"))
+      bInSessions = true;
     else
       break;
   }
@@ -47,7 +53,6 @@ int main(int argc,char **argv)
   }
 
   line_no = 0;
-  balance = 0;
 
   for ( ; ; ) {
     fscanf(fptr,"%s\t%d",str,&delta);
@@ -55,26 +60,58 @@ int main(int argc,char **argv)
     if (feof(fptr))
       break;
 
-    line_no++;
+    if (!line_no) {
+      if (delta < 0) {
+        max_balance = delta * -1;
+        max_balance_ix = -1;
+        balance = 0;
+      }
+      else {
+        max_balance = delta;
+        max_balance_ix = 0;
+        balance = max_balance;
+      }
+    }
+    else {
+      balance += delta;
 
-    balance += delta;
-
-    if ((line_no == 1) || (balance > max_balance))
-      max_balance = balance;
+      if (balance > max_balance) {
+        max_balance = balance;
+        max_balance_ix = line_no;
+      }
+    }
 
     if (!bTerse) {
-      if (!bNoDates)
-        printf("%s\t%d\n",str,max_balance - balance);
-      else
-        printf("%d\n",max_balance - balance);
+      if (!bNoDates) {
+        if (!bInSessions)
+          printf("%s\t%d\n",str,max_balance - balance);
+        else
+          printf("%s\t%d\n",str,line_no - max_balance_ix);
+      }
+      else {
+        if (!bInSessions)
+          printf("%d\n",max_balance - balance);
+        else
+          printf("%d\n",line_no - max_balance_ix);
+      }
     }
+
+    line_no++;
   }
 
   if (bTerse) {
-    if (!bNoDates)
-      printf("%s\t%d\n",str,max_balance - balance);
-    else
-      printf("%d\n",max_balance - balance);
+    if (!bNoDates) {
+      if (!bInSessions)
+        printf("%s\t%d\n",str,max_balance - balance);
+      else
+        printf("%s\t%d\n",str,line_no - max_balance_ix);
+    }
+    else {
+      if (!bInSessions)
+        printf("%d\n",max_balance - balance);
+      else
+        printf("%d\n",line_no - max_balance_ix);
+    }
   }
 
   fclose(fptr);
