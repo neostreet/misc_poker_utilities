f44c55c942e1ee9c49a5346b18858f12e9fcc7cd
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-09-18 16:30:04 -0400

    add decisions framework

--- fskeleton3.c
+++ fskeleton3.c
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +7,6 @@
 #define _MAX_PATH 4096
 #include <unistd.h>
 #endif
-using namespace std;
 
 #define MAX_FILENAME_LEN 1024
 static char filename[MAX_FILENAME_LEN];
@@ -17,52 +15,64 @@ static char filename[MAX_FILENAME_LEN];
 static char line[MAX_LINE_LEN];
 
 static char usage[] =
-"usage: fskeleton3 (-terse) (-verbose) (-debug) filename\n";
+"usage: fskeleton3 (-verbose) (-debug) player_name filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static char in_chips[] = " in chips";
 #define IN_CHIPS_LEN (sizeof (in_chips) - 1)
+static char summary[] = "*** SUMMARY ***";
+#define SUMMARY_LEN (sizeof (summary) - 1)
+static char dealt_to[] = "Dealt to ";
+#define DEALT_TO_LEN (sizeof (dealt_to) - 1)
+static char folds[] = " folds ";
+#define FOLDS_LEN (sizeof (folds) - 1)
+static char bets[] = " bets ";
+#define BETS_LEN (sizeof (bets) - 1)
+static char calls[] = " calls ";
+#define CALLS_LEN (sizeof (calls) - 1)
+static char raises[] = " raises ";
+#define RAISES_LEN (sizeof (raises) - 1)
+static char checks[] = " checks ";
+#define CHECKS_LEN (sizeof (checks) - 1)
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 static int Contains(bool bCaseSens,char *line,int line_len,
   char *string,int string_len,int *index);
-static void normalize_hole_cards(char *hole_cards);
 
 int main(int argc,char **argv)
 {
   int m;
   int n;
   int p;
-  int q;
   int curr_arg;
-  bool bTerse;
   bool bVerbose;
   bool bDebug;
+  int player_name_ix;
+  int player_name_len;
   FILE *fptr0;
   int filename_len;
   FILE *fptr;
   int line_len;
   int line_no;
   int dbg_line_no;
+  int ix;
   int file_no;
   int dbg_file_no;
   int num_hands;
   int dbg;
-  int work;
+  char hole_cards[6];
+  bool bSkipping;
 
-  if ((argc < 2) || (argc > 5)) {
+  if ((argc < 3) || (argc > 5)) {
     printf(usage);
     return 1;
   }
 
-  bTerse = false;
   bVerbose = false;
   bDebug = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-terse"))
-      bTerse = true;
-    else if (!strcmp(argv[curr_arg],"-verbose"))
+    if (!strcmp(argv[curr_arg],"-verbose"))
       bVerbose = true;
     else if (!strcmp(argv[curr_arg],"-debug"))
       bDebug = true;
@@ -70,23 +80,23 @@ int main(int argc,char **argv)
       break;
   }
 
-  if (argc - curr_arg != 1) {
+  if (argc - curr_arg != 2) {
     printf(usage);
     return 2;
   }
 
-  if (bTerse && bVerbose) {
-    printf("can't specify both -terse and -verbose\n");
-    return 3;
-  }
+  player_name_ix = curr_arg++;
+  player_name_len = strlen(argv[player_name_ix]);
 
   if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
     printf(couldnt_open,argv[curr_arg]);
-    return 4;
+    return 3;
   }
 
   file_no = 0;
   dbg_file_no = -1;
+
+  hole_cards[5] = 0;
 
   for ( ; ; ) {
     GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
@@ -105,6 +115,7 @@ int main(int argc,char **argv)
     }
 
     line_no = 0;
+    bSkipping = false;
     num_hands = 0;
 
     for ( ; ; ) {
@@ -120,6 +131,104 @@ int main(int argc,char **argv)
 
       if (bDebug)
         printf("line %d %s\n",line_no,line);
+
+      if (Contains(true,
+        line,line_len,
+        argv[player_name_ix],player_name_len,
+        &ix)) {
+
+        if (Contains(true,
+          line,line_len,
+          in_chips,IN_CHIPS_LEN,
+          &ix)) {
+
+          num_hands++;
+          bSkipping = false;
+
+          line[ix] = 0;
+
+          for (ix--; (ix >= 0) && (line[ix] != '('); ix--)
+            ;
+
+          continue;
+        }
+        else if (bSkipping)
+          continue;
+        else if (!strncmp(line,dealt_to,DEALT_TO_LEN)) {
+          for (n = 0; n < line_len; n++) {
+            if (line[n] == '[')
+              break;
+          }
+
+          if (n < line_len) {
+            n++;
+
+            for (m = n; m < line_len; m++) {
+              if (line[m] == ']')
+                break;
+            }
+
+            if (m < line_len) {
+              for (p = 0; p < 5; p++) {
+                if (line[n+p] == ']')
+                  break;
+                else
+                  hole_cards[p] = line[n+p];
+              }
+            }
+          }
+        }
+        else if (Contains(true,
+          line,line_len,
+          folds,FOLDS_LEN,
+          &ix)) {
+
+          continue;
+        }
+        else if (Contains(true,
+          line,line_len,
+          bets,BETS_LEN,
+          &ix)) {
+          ;
+        }
+        else if (Contains(true,
+          line,line_len,
+          calls,CALLS_LEN,
+          &ix)) {
+          ;
+        }
+        else if (Contains(true,
+          line,line_len,
+          raises,RAISES_LEN,
+          &ix)) {
+          ;
+        }
+        else if (Contains(true,
+          line,line_len,
+          checks,CHECKS_LEN,
+          &ix)) {
+          ;
+        }
+      }
+      else if (bSkipping)
+        continue;
+      else {
+        if (!strncmp(line,summary,SUMMARY_LEN)) {
+          if (bDebug)
+            printf("line %d SUMMARY line detected; skipping\n",line_no);
+
+          bSkipping = true;
+
+          printf("%s",hole_cards);
+
+          if (bVerbose)
+            printf(" %s %3d\n",filename,num_hands);
+          else
+            putchar(0x0a);
+
+          continue;
+        }
+      }
     }
 
     fclose(fptr);
