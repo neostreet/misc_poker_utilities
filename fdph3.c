1e4e51b13838eb40ecc922559dd99eec8d9ec74b
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-09-18 17:44:02 -0400

    add fdph3.c

---
+++ fdph3.c
@@ -0,0 +1,316 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: fdph3 (-verbose) (-debug) player_name filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static char in_chips[] = " in chips";
+#define IN_CHIPS_LEN (sizeof (in_chips) - 1)
+static char summary[] = "*** SUMMARY ***";
+#define SUMMARY_LEN (sizeof (summary) - 1)
+static char dealt_to[] = "Dealt to ";
+#define DEALT_TO_LEN (sizeof (dealt_to) - 1)
+static char folds[] = " folds ";
+#define FOLDS_LEN (sizeof (folds) - 1)
+static char bets[] = " bets ";
+#define BETS_LEN (sizeof (bets) - 1)
+static char calls[] = " calls ";
+#define CALLS_LEN (sizeof (calls) - 1)
+static char raises[] = " raises ";
+#define RAISES_LEN (sizeof (raises) - 1)
+static char checks[] = " checks ";
+#define CHECKS_LEN (sizeof (checks) - 1)
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+
+int main(int argc,char **argv)
+{
+  int m;
+  int n;
+  int p;
+  int curr_arg;
+  bool bVerbose;
+  bool bDebug;
+  int player_name_ix;
+  int player_name_len;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int dbg_line_no;
+  int ix;
+  int file_no;
+  int dbg_file_no;
+  int total_num_decisions;
+  int total_num_hands;
+  int num_decisions;
+  int num_hands;
+  int dbg;
+  char hole_cards[6];
+  bool bSkipping;
+  double dwork;
+
+  if ((argc < 3) || (argc > 5)) {
+    printf(usage);
+    return 1;
+  }
+
+  bVerbose = false;
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 2) {
+    printf(usage);
+    return 2;
+  }
+
+  player_name_ix = curr_arg++;
+  player_name_len = strlen(argv[player_name_ix]);
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  file_no = 0;
+  dbg_file_no = -1;
+
+  hole_cards[5] = 0;
+
+  total_num_decisions = 0;
+  total_num_hands = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    file_no++;
+
+    if (dbg_file_no == file_no)
+      dbg = 1;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    line_no = 0;
+    bSkipping = false;
+    num_hands = 0;
+    num_decisions = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      line_no++;
+
+      if (line_no == dbg_line_no)
+        dbg = 1;
+
+      if (bDebug)
+        printf("line %d %s\n",line_no,line);
+
+      if (Contains(true,
+        line,line_len,
+        argv[player_name_ix],player_name_len,
+        &ix)) {
+
+        if (Contains(true,
+          line,line_len,
+          in_chips,IN_CHIPS_LEN,
+          &ix)) {
+
+          num_hands++;
+          bSkipping = false;
+          num_decisions = 0;
+
+          line[ix] = 0;
+
+          for (ix--; (ix >= 0) && (line[ix] != '('); ix--)
+            ;
+        }
+        else if (bSkipping)
+          ;
+        else if (!strncmp(line,dealt_to,DEALT_TO_LEN)) {
+          for (n = 0; n < line_len; n++) {
+            if (line[n] == '[')
+              break;
+          }
+
+          if (n < line_len) {
+            n++;
+
+            for (m = n; m < line_len; m++) {
+              if (line[m] == ']')
+                break;
+            }
+
+            if (m < line_len) {
+              for (p = 0; p < 5; p++) {
+                if (line[n+p] == ']')
+                  break;
+                else
+                  hole_cards[p] = line[n+p];
+              }
+            }
+          }
+        }
+        else if (Contains(true,
+          line,line_len,
+          folds,FOLDS_LEN,
+          &ix)) {
+
+          num_decisions++;
+        }
+        else if (Contains(true,
+          line,line_len,
+          bets,BETS_LEN,
+          &ix)) {
+
+          num_decisions++;
+        }
+        else if (Contains(true,
+          line,line_len,
+          calls,CALLS_LEN,
+          &ix)) {
+
+          num_decisions++;
+        }
+        else if (Contains(true,
+          line,line_len,
+          raises,RAISES_LEN,
+          &ix)) {
+
+          num_decisions++;
+        }
+        else if (Contains(true,
+          line,line_len,
+          checks,CHECKS_LEN,
+          &ix)) {
+
+          num_decisions++;
+        }
+      }
+      else if (bSkipping)
+        ;
+      else {
+        if (!strncmp(line,summary,SUMMARY_LEN)) {
+          if (bDebug)
+            printf("line %d SUMMARY line detected; skipping\n",line_no);
+
+          bSkipping = true;
+
+          if (bVerbose) {
+            printf("%10d %s %s %3d\n",num_decisions,hole_cards,
+              filename,num_hands);
+          }
+
+          total_num_decisions += num_decisions;
+          total_num_hands++;
+        }
+      }
+    }
+
+    fclose(fptr);
+  }
+
+  fclose(fptr0);
+
+  dwork = (double)total_num_decisions / (double)total_num_hands;
+
+  if (bVerbose)
+    putchar(0x0a);
+
+  printf("%10d %10d %5.2lf\n",total_num_decisions,total_num_hands,dwork);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
