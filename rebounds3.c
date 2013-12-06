b30ef1f6f9b62cd0c047f76d540e0ace78a9f551
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-12-05 20:48:09 -0500

    add rebounds3.c

---
+++ rebounds3.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: rebounds3 filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int val;
+  int old_val;
+  int rebound;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%d",&val);
+
+    if ((line[line_len - 2] == ' ') && (line[line_len - 1] == '1'))
+      ;
+    else if ((line_no > 1) && (old_val < 0) && (val > 0)) {
+      if (val > old_val * -1) {
+        rebound = val - old_val;
+        printf("%10d %s\n",rebound,line);
+      }
+    }
+
+    old_val = val;
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
