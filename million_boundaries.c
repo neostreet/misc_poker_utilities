d4c67a040f4bbcf5f446d4f439c97f80e12fac48
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-02-24 21:25:00 -0500

    add -debug flag

--- million_boundaries.c
+++ million_boundaries.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: million_boundaries filename\n";
+static char usage[] = "usage: million_boundaries (-debug) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  int curr_arg;
+  bool bDebug;
   FILE *fptr;
   int line_len;
   int line_no;
@@ -19,14 +22,28 @@ int main(int argc,char **argv)
   int starting_balance;
   int ending_balance;
 
-  if (argc != 2) {
+  if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
     return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
   }
 
   line_no = 0;
@@ -44,8 +61,14 @@ int main(int argc,char **argv)
     sscanf(line,"%s\t%d\t%d",&date_str[0],&starting_balance,&ending_balance);
 
     if ((starting_balance < boundary) && (boundary < ending_balance)) {
-      printf("%s\t%d\t%d (%d)\n",date_str,starting_balance,ending_balance,
-        line_no - last_boundary_ix);
+      if (!bDebug) {
+        printf("%s\t%d\t%d (%d)\n",date_str,starting_balance,ending_balance,
+          line_no - last_boundary_ix);
+      }
+      else {
+        printf("%s\t%d\t%d (%d %d %d)\n",date_str,starting_balance,ending_balance,
+          last_boundary_ix,line_no,line_no - last_boundary_ix);
+      }
 
       last_boundary_ix = line_no;
       boundary += 1000000;
