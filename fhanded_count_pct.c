6cae05ecfabd119465edbd4117480a702a489746
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-03-02 09:45:28 -0500

    add fhanded_count_pct.c

---
+++ fhanded_count_pct.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: fhanded_count_pct (-terse) (-verbose) handed_count filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bTerse;
+  bool bVerbose;
+  int handed_count;
+  FILE *fptr0;
+  int filename_len;
+  int num_files;
+  FILE *fptr;
+  int line_len;
+  int table_count;
+  int num_hands;
+  int num_handed_count_hands;
+  double handed_count_pct;
+  int curr_file_num_hands;
+
+  if ((argc < 2) || (argc > 5)) {
+    printf(usage);
+    return 1;
+  }
+
+  bTerse = false;
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-terse"))
+      bTerse = true;
+    else if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 2) {
+    printf(usage);
+    return 2;
+  }
+
+  sscanf(argv[curr_arg++],"%d",&handed_count);
+
+  if (bTerse && bVerbose) {
+    printf("can't specify both -terse and -verbose\n");
+    return 3;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 4;
+  }
+
+  num_files = 0;
+  num_hands = 0;
+  num_handed_count_hands = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    curr_file_num_hands = 0;
+    num_files++;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      if (!strncmp(line,"Table '",7)) {
+        table_count = 0;
+        curr_file_num_hands++;
+
+        for ( ; ; ) {
+          GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+          if (feof(fptr))
+            break;
+
+          if (!strncmp(line,"*** HOLE CARDS ***",18))
+            break;
+
+          if (!strncmp(line,"Seat ",5))
+            table_count++;
+        }
+
+        if (table_count == handed_count) {
+          num_handed_count_hands++;
+
+          if (bVerbose)
+            printf("%s %d\n",filename,curr_file_num_hands);
+        }
+      }
+    }
+
+    fclose(fptr);
+
+    num_hands += curr_file_num_hands;
+  }
+
+  fclose(fptr0);
+
+  handed_count_pct = (double)num_handed_count_hands / (double)num_hands;
+
+  if (bTerse)
+    printf("%lf\n",handed_count_pct);
+  else
+    printf("%lf (%d of %d)\n",handed_count_pct,num_handed_count_hands,num_hands);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
