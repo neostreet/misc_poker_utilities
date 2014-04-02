e0375aab4cf7023948c422900d6d71fd9fcd7e84
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-04-01 22:37:07 -0400

    remove -summary flag; add -verbose and -right_justify flags

--- gen_wifs.c
+++ gen_wifs.c
@@ -4,29 +4,29 @@
 #define NUM_PLACES 6
 
 static char usage[] =
-"usage: gen_wifs (-summary) tournament_info_file\n";
+"usage: gen_wifs (-verbose) (-right_justify) tournament_info_file\n";
 
 int main(int argc,char **argv)
 {
   int curr_arg;
-  bool bSummary;
+  bool bVerbose;
+  bool bRightJustify;
   int m;
   int n;
-  int o;
-  int p;
-  int q;
-  int r;
 
-  if ((argc < 2) || (argc > 3)) {
+  if ((argc < 2) || (argc > 4)) {
     printf(usage);
     return 1;
   }
 
-  bSummary = false;
+  bVerbose = false;
+  bRightJustify = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-summary"))
-      bSummary = true;
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else if (!strcmp(argv[curr_arg],"-right_justify"))
+      bRightJustify = true;
     else
       break;
   }
@@ -38,30 +38,21 @@ int main(int argc,char **argv)
 
   printf("@echo off\n\n");
 
-  if (bSummary) {
-    for (m = 0; m <= NUM_PLACES; m++) {
-      for (n = 0; n <= NUM_PLACES; n++) {
-        if (m + n <= NUM_PLACES) {
-          printf("tournament_wif -right_justify %s %d %d %d\n",
-            argv[curr_arg],m,n,NUM_PLACES);
-        }
-      }
-    }
-  }
-  else {
-    for (m = 0; m <= NUM_PLACES; m++) {
-      for (n = 0; n <= NUM_PLACES; n++) {
-        for (o = 0; o <= NUM_PLACES; o++) {
-          for (p = 0; p <= NUM_PLACES; p++) {
-            for (q = 0; q <= NUM_PLACES; q++) {
-              for (r = 0; r <= NUM_PLACES; r++) {
-                if (m + n + o + p + q + r <= NUM_PLACES) {
-                  printf("tournament_wif -right_justify %s %d %d %d\n",
-                    argv[curr_arg],m,n,NUM_PLACES);
-                }
-              }
-            }
+  for (m = 0; m <= NUM_PLACES; m++) {
+    for (n = 0; n <= NUM_PLACES; n++) {
+      if (m + n <= NUM_PLACES) {
+        if (!bVerbose) {
+          if (!bRightJustify)
+            printf("tournament_wif %s %d %d %d\n",
+              argv[curr_arg],m,n,NUM_PLACES);
+          else {
+            printf("tournament_wif -right_justify %s %d %d %d\n",
+              argv[curr_arg],m,n,NUM_PLACES);
           }
+        }
+        else {
+          printf("tournament_wif -verbose %s %d %d %d\n",
+            argv[curr_arg],m,n,NUM_PLACES);
         }
       }
     }
