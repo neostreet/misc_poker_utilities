64dcff316fa32228410a019825c024dab59b429b
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-04-01 22:52:51 -0400

    add num_tournaments argument

--- gen_wifs.c
+++ gen_wifs.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-#define NUM_PLACES 6
-
 static char usage[] =
-"usage: gen_wifs (-verbose) (-right_justify) tournament_info_file\n";
+"usage: gen_wifs (-verbose) (-right_justify) tournament_info_file num_tournaments\n";
 
 int main(int argc,char **argv)
 {
   int curr_arg;
+  int num_tournaments;
   bool bVerbose;
   bool bRightJustify;
   int m;
   int n;
 
-  if ((argc < 2) || (argc > 4)) {
+  if ((argc < 3) || (argc > 5)) {
     printf(usage);
     return 1;
   }
@@ -31,28 +30,30 @@ int main(int argc,char **argv)
       break;
   }
 
-  if (argc - curr_arg != 1) {
+  if (argc - curr_arg != 2) {
     printf(usage);
     return 2;
   }
 
+  sscanf(argv[curr_arg+1],"%d",&num_tournaments);
+
   printf("@echo off\n\n");
 
-  for (m = 0; m <= NUM_PLACES; m++) {
-    for (n = 0; n <= NUM_PLACES; n++) {
-      if (m + n <= NUM_PLACES) {
+  for (m = 0; m <= num_tournaments; m++) {
+    for (n = 0; n <= num_tournaments; n++) {
+      if (m + n <= num_tournaments) {
         if (!bVerbose) {
           if (!bRightJustify)
             printf("tournament_wif %s %d %d %d\n",
-              argv[curr_arg],m,n,NUM_PLACES);
+              argv[curr_arg],m,n,num_tournaments);
           else {
             printf("tournament_wif -right_justify %s %d %d %d\n",
-              argv[curr_arg],m,n,NUM_PLACES);
+              argv[curr_arg],m,n,num_tournaments);
           }
         }
         else {
           printf("tournament_wif -verbose %s %d %d %d\n",
-            argv[curr_arg],m,n,NUM_PLACES);
+            argv[curr_arg],m,n,num_tournaments);
         }
       }
     }
