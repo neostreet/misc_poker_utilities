65b38eaf181db8244bc0dcbfa6d0e86115bf394a
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-04-04 22:53:09 -0400

    add feliminations.c

---
+++ feliminations.c
@@ -0,0 +1,450 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: feliminations (-terse) (-verbose) (-debug) player_name filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static char in_chips[] = " in chips";
+#define IN_CHIPS_LEN (sizeof (in_chips) - 1)
+static char summary[] = "*** SUMMARY ***";
+#define SUMMARY_LEN (sizeof (summary) - 1)
+static char street_marker[] = "*** ";
+#define STREET_MARKER_LEN (sizeof (street_marker) - 1)
+static char posts[] = " posts ";
+#define POSTS_LEN (sizeof (posts) - 1)
+static char folds[] = " folds ";
+#define FOLDS_LEN (sizeof (folds) - 1)
+static char bets[] = " bets ";
+#define BETS_LEN (sizeof (bets) - 1)
+static char calls[] = " calls ";
+#define CALLS_LEN (sizeof (calls) - 1)
+static char raises[] = " raises ";
+#define RAISES_LEN (sizeof (raises) - 1)
+static char uncalled_bet[] = "Uncalled bet (";
+#define UNCALLED_BET_LEN (sizeof (uncalled_bet) - 1)
+static char collected[] = " collected ";
+#define COLLECTED_LEN (sizeof (collected) - 1)
+static char finished_the_tournament[] = " finished the tournament ";
+#define FINISHED_THE_TOURNAMENT_LEN (sizeof (finished_the_tournament) - 1)
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+int get_work_amount(char *line,int line_len);
+
+int main(int argc,char **argv)
+{
+  int m;
+  int n;
+  int p;
+  int curr_arg;
+  bool bTerse;
+  bool bVerbose;
+  bool bDebug;
+  int player_name_ix;
+  int player_name_len;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int dbg_line_no;
+  int ix;
+  int street;
+  int num_street_markers;
+  int starting_balance;
+  int spent_this_street;
+  int spent_this_hand;
+  int end_ix;
+  int uncalled_bet_amount;
+  int collected_from_pot;
+  int collected_from_pot_count;
+  int ending_balance;
+  int delta;
+  int file_no;
+  int dbg_file_no;
+  int num_hands;
+  int dbg;
+  int work;
+  double dwork1;
+  double dwork2;
+  double sum_deltas_dwork;
+  int sum_deltas;
+  int sum_positive_deltas;
+  int sum_negative_deltas;
+  int sum_absolute_value_deltas;
+  int retval;
+  int curr_big_blind;
+  int last_big_blind;
+  int num_finished;
+  int num_eliminations;
+
+  if ((argc < 3) || (argc > 6)) {
+    printf(usage);
+    return 1;
+  }
+
+  bTerse = false;
+  bVerbose = false;
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-terse"))
+      bTerse = true;
+    else if (!strcmp(argv[curr_arg],"-verbose")) {
+      bVerbose = true;
+      getcwd(save_dir,_MAX_PATH);
+    }
+    else if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 2) {
+    printf(usage);
+    return 2;
+  }
+
+  if (bTerse && bVerbose) {
+    printf("can't specify both -terse and -verbose\n");
+    return 3;
+  }
+
+  player_name_ix = curr_arg++;
+  player_name_len = strlen(argv[player_name_ix]);
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 5;
+  }
+
+  ending_balance = -1;
+
+  file_no = 0;
+  dbg_file_no = -1;
+  num_hands = 0;
+  num_eliminations = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    file_no++;
+
+    if (dbg_file_no == file_no)
+      dbg = 1;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    num_hands++;
+
+    line_no = 0;
+    street = 0;
+    num_street_markers = 0;
+    spent_this_street = 0;
+    spent_this_hand = 0;
+    uncalled_bet_amount = 0;
+    collected_from_pot = 0;
+    collected_from_pot_count = 0;
+    num_finished = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      line_no++;
+
+      if (line_no == dbg_line_no)
+        dbg = 1;
+
+      if (bDebug)
+        printf("line %d %s\n",line_no,line);
+
+      if (Contains(true,
+        line,line_len,
+        argv[player_name_ix],player_name_len,
+        &ix)) {
+
+        if (Contains(true,
+          line,line_len,
+          in_chips,IN_CHIPS_LEN,
+          &ix)) {
+
+          line[ix] = 0;
+
+          for (ix--; (ix >= 0) && (line[ix] != '('); ix--)
+            ;
+
+          sscanf(&line[ix+1],"%d",&starting_balance);
+
+          if (bDebug)
+            printf("line %d starting_balance = %d\n",line_no,starting_balance);
+
+          continue;
+        }
+        else if (Contains(true,
+          line,line_len,
+          posts,POSTS_LEN,
+          &ix)) {
+          work = get_work_amount(line,line_len);
+          spent_this_street += work;
+
+          if (bDebug) {
+            printf("line %d street %d POSTS work = %d, spent_this_street = %d\n",
+              line_no,street,work,spent_this_street);
+          }
+
+          continue;
+        }
+        else if (Contains(true,
+          line,line_len,
+          collected,COLLECTED_LEN,
+          &ix)) {
+
+          for (end_ix = ix + COLLECTED_LEN; end_ix < line_len; end_ix++) {
+            if (line[end_ix] == ' ')
+              break;
+          }
+
+          line[end_ix] = 0;
+          sscanf(&line[ix + COLLECTED_LEN],"%d",&work);
+
+          if (!collected_from_pot_count) {
+            spent_this_hand += spent_this_street;
+            street++;
+            spent_this_street = 0;
+          }
+
+          collected_from_pot += work;
+          collected_from_pot_count++;
+
+          if (bDebug) {
+            printf("line %d street %d COLLECTED work = %d, collected_from_pot = %d\n",
+              line_no,street,work,collected_from_pot);
+          }
+
+          continue;
+        }
+        else if (!strncmp(line,uncalled_bet,UNCALLED_BET_LEN)) {
+          sscanf(&line[UNCALLED_BET_LEN],"%d",&uncalled_bet_amount);
+          spent_this_street -= uncalled_bet_amount;
+
+          if (bDebug) {
+            printf("line %d street %d UNCALLED uncalled_bet_amount = %d, spent_this_street = %d\n",
+              line_no,street,uncalled_bet_amount,spent_this_street);
+          }
+
+          continue;
+        }
+        else if (Contains(true,
+          line,line_len,
+          folds,FOLDS_LEN,
+          &ix)) {
+          spent_this_hand += spent_this_street;
+
+          if (bDebug) {
+            printf("line %d street %d FOLDS spent_this_street = %d, spent_this_hand = %d\n",
+              line_no,street,spent_this_street,spent_this_hand);
+          }
+
+          break;
+        }
+        else if (Contains(true,
+          line,line_len,
+          bets,BETS_LEN,
+          &ix)) {
+          work = get_work_amount(line,line_len);
+          spent_this_street += work;
+
+          if (bDebug) {
+            printf("line %d street %d BETS work = %d, spent_this_street = %d\n",
+              line_no,street,work,spent_this_street);
+          }
+        }
+        else if (Contains(true,
+          line,line_len,
+          calls,CALLS_LEN,
+          &ix)) {
+          work = get_work_amount(line,line_len);
+          spent_this_street += work;
+
+          if (bDebug) {
+            printf("line %d street %d CALLS work = %d, spent_this_street = %d\n",
+              line_no,street,work,spent_this_street);
+          }
+        }
+        else if (Contains(true,
+          line,line_len,
+          raises,RAISES_LEN,
+          &ix)) {
+          work = get_work_amount(line,line_len);
+          spent_this_street = work;
+
+          if (bDebug) {
+            printf("line %d street %d RAISES work = %d, spent_this_street = %d\n",
+              line_no,street,work,spent_this_street);
+          }
+        }
+      }
+      else {
+        if (Contains(true,
+          line,line_len,
+          finished_the_tournament,FINISHED_THE_TOURNAMENT_LEN,
+          &ix)) {
+          num_finished++;
+        }
+        else if (!strncmp(line,summary,SUMMARY_LEN)) {
+          if (bDebug)
+            printf("line %d SUMMARY line detected; breaking\n",line_no);
+
+          break;
+        }
+
+        if (!strncmp(line,street_marker,STREET_MARKER_LEN)) {
+          num_street_markers++;
+
+          if (num_street_markers > 1) {
+            if (street <= 3)
+              spent_this_hand += spent_this_street;
+
+            street++;
+            spent_this_street = 0;
+          }
+        }
+      }
+    }
+
+    fclose(fptr);
+
+    ending_balance = starting_balance - spent_this_hand + collected_from_pot;
+    delta = ending_balance - starting_balance;
+
+    if (delta > 0) {
+      num_eliminations += num_finished;
+    }
+  }
+
+  fclose(fptr0);
+
+  if (num_eliminations) {
+    if (!bVerbose)
+      printf("%d\n",num_eliminations);
+    else
+      printf("%d %s\n",num_eliminations,save_dir);
+  }
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
+
+int get_work_amount(char *line,int line_len)
+{
+  int ix;
+  int chara;
+  int work_amount;
+
+  work_amount = 0;
+
+  for (ix = line_len - 1; (ix >= 0); ix--) {
+    chara = line[ix];
+
+    if ((chara >= '0') && (chara <= '9'))
+      break;
+  }
+
+  if (ix + 1 != line_len);
+    line[ix + 1] = 0;
+
+  for ( ; (ix >= 0); ix--) {
+    chara = line[ix];
+
+    if ((chara < '0') || (chara > '9'))
+      break;
+  }
+
+  sscanf(&line[ix+1],"%d",&work_amount);
+
+  return work_amount;
+}
