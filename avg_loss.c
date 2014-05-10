ca250bdc0dd71c225ee72eba343ee36deb0e87a4
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-05-09 21:49:44 -0400

    add avg_loss.c and avg_gain.c

---
+++ avg_loss.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: avg_loss (-debug) (-verbose) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bDebug;
+  bool bVerbose;
+  FILE *fptr;
+  int delta;
+  int num_losses;
+  int total_losses;
+  double avg_loss;
+  int line_len;
+  int line_no;
+
+  if ((argc < 2) || (argc > 4)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug")) {
+      bDebug = true;
+      getcwd(save_dir,_MAX_PATH);
+    }
+    else if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  line_no = 0;
+  num_losses = 0;
+  total_losses = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%d",&delta);
+
+    if (delta < 0) {
+      num_losses++;
+      total_losses += delta;
+    }
+  }
+
+  avg_loss = (double)total_losses / (double)num_losses;
+
+  if (!bVerbose) {
+    if (!bDebug)
+      printf("%lf\n",avg_loss);
+    else
+      printf("%s %lf\n",save_dir,avg_loss);
+  }
+  else {
+    if (!bDebug)
+      printf("%lf (%d %d)\n",avg_loss,total_losses,line_no);
+    else
+      printf("%s %lf (%d %d)\n",save_dir,avg_loss,total_losses,line_no);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
