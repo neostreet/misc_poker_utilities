32ac913a8cdf32cb3800a88240bdc84b0e0f7b3a
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-05-17 11:28:59 -0400

    add amount won to the output; use get_date_from_path() for the dates

--- first_hand_win.c
+++ first_hand_win.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #ifdef WIN32
 #include <direct.h>
@@ -12,36 +13,32 @@ static char save_dir[_MAX_PATH];
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: first_hand_win (-debug) (-not) filename\n";
+static char usage[] = "usage: first_hand_win (-not) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int get_date_from_path(char *path,char slash_char,int num_slashes,char **date_string_ptr);
 
 int main(int argc,char **argv)
 {
   int curr_arg;
-  bool bDebug;
   bool bNot;
-  bool bFirstHandWin;
   FILE *fptr;
   int line_len;
-  int line_no;
-  int work;
+  int retval;
+  char *date_string;
+  int val;
+  bool bHaveVal;
 
-  if ((argc < 2) || (argc > 4)) {
+  if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
-  bDebug = false;
   bNot = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-debug")) {
-      bDebug = true;
-      getcwd(save_dir,_MAX_PATH);
-    }
-    else if (!strcmp(argv[curr_arg],"-not"))
+    if (!strcmp(argv[curr_arg],"-not"))
       bNot = true;
     else
       break;
@@ -57,35 +54,28 @@ int main(int argc,char **argv)
     return 3;
   }
 
-  line_no = 0;
+  getcwd(save_dir,_MAX_PATH);
 
-  for ( ; ; ) {
-    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+  retval = get_date_from_path(save_dir,'/',2,&date_string);
 
-    if (feof(fptr))
-      break;
-
-    line_no++;
-
-    sscanf(line,"%d",&work);
-
-    if (work > 0)
-      bFirstHandWin = true;
-    else
-      bFirstHandWin = false;
-
-    break;
+  if (retval) {
+    printf("get_date_from_path() failed: %d\n",retval);
+    return 4;
   }
+
+  GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+  if (feof(fptr)) {
+    printf("no lines in file %s\n",argv[curr_arg]);
+    return 5;
+  }
+
+  sscanf(line,"%d",&val);
 
   fclose(fptr);
 
-  if ((!bNot && bFirstHandWin) ||
-      (bNot && !bFirstHandWin)) {
-    if (!bDebug)
-      printf("true\n");
-    else
-      printf("%s\n",save_dir);
-  }
+  if ((!bNot && (val > 0)) || (bNot && (val < 0)))
+    printf("%10d %s\n",val,date_string);
 
   return 0;
 }
@@ -112,4 +102,42 @@ static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
 
   line[local_line_len] = 0;
   *line_len = local_line_len;
+}
+
+static char sql_date_string[11];
+
+static int get_date_from_path(char *path,char slash_char,int num_slashes,char **date_string_ptr)
+{
+  int n;
+  int len;
+  int slash_count;
+
+  len = strlen(path);
+  slash_count = 0;
+
+  for (n = len - 1; (n >= 0); n--) {
+    if (path[n] == slash_char) {
+      slash_count++;
+
+      if (slash_count == num_slashes)
+        break;
+    }
+  }
+
+  if (slash_count != num_slashes)
+    return 1;
+
+  if (path[n+5] != slash_char)
+    return 2;
+
+  strncpy(sql_date_string,&path[n+1],4);
+  sql_date_string[4] = '-';
+  strncpy(&sql_date_string[5],&path[n+6],2);
+  sql_date_string[7] = '-';
+  strncpy(&sql_date_string[8],&path[n+8],2);
+  sql_date_string[10] = 0;
+
+  *date_string_ptr = sql_date_string;
+
+  return 0;
 }
