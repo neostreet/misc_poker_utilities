5d99ac404c7c89bb997c14adf34d8ccad1323cd3
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-06-19 20:59:37 -0400

    add get_date_from_cwd() call

--- max_balance.c
+++ max_balance.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
 
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
@@ -8,11 +17,14 @@ static char usage[] = "usage: max_balance (-initial_balbal) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int get_date_from_cwd(char *cwd,char **date_string_ptr);
 
 int main(int argc,char **argv)
 {
   int curr_arg;
   FILE *fptr;
+  int retval;
+  char *date_string;
   int line_len;
   int line_no;
   int delta;
@@ -43,6 +55,15 @@ int main(int argc,char **argv)
     return 3;
   }
 
+  getcwd(save_dir,_MAX_PATH);
+
+  retval = get_date_from_cwd(save_dir,&date_string);
+
+  if (retval) {
+    printf("get_date_from_cwd() failed: %d\n",retval);
+    return 4;
+  }
+
   line_no = 0;
 
   for ( ; ; ) {
@@ -63,7 +84,7 @@ int main(int argc,char **argv)
 
   fclose(fptr);
 
-  printf("%d\n",max_balance);
+  printf("%10d %s\n",max_balance,date_string);
 
   return 0;
 }
@@ -90,4 +111,42 @@ static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
 
   line[local_line_len] = 0;
   *line_len = local_line_len;
+}
+
+static char sql_date_string[11];
+
+static int get_date_from_cwd(char *cwd,char **date_string_ptr)
+{
+  int n;
+  int len;
+  int slash_count;
+
+  len = strlen(cwd);
+  slash_count = 0;
+
+  for (n = len - 1; (n >= 0); n--) {
+    if (cwd[n] == '/') {
+      slash_count++;
+
+      if (slash_count == 2)
+        break;
+    }
+  }
+
+  if (slash_count != 2)
+    return 1;
+
+  if (cwd[n+5] != '/')
+    return 2;
+
+  strncpy(sql_date_string,&cwd[n+1],4);
+  sql_date_string[4] = '-';
+  strncpy(&sql_date_string[5],&cwd[n+6],2);
+  sql_date_string[7] = '-';
+  strncpy(&sql_date_string[8],&cwd[n+8],2);
+  sql_date_string[10] = 0;
+
+  *date_string_ptr = sql_date_string;
+
+  return 0;
 }
