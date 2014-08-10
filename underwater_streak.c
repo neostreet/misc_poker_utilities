a72f15f3e4496e380f1874e9f7a74b1fd9295b7d
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-08-09 23:16:23 -0400

    add underwater_streak.c

---
+++ underwater_streak.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: underwater_streak (-debug) (-only_winning) (-only_losing) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bDebug;
+  bool bOnlyWinning;
+  bool bOnlyLosing;
+  int val;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int count;
+  int work;
+  int max_streak;
+
+  if ((argc < 2) || (argc > 5)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+  bOnlyWinning = false;
+  bOnlyLosing = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug")) {
+      bDebug = true;
+      getcwd(save_dir,_MAX_PATH);
+    }
+    else if (!strcmp(argv[curr_arg],"-only_winning"))
+      bOnlyWinning = true;
+    else if (!strcmp(argv[curr_arg],"-only_losing"))
+      bOnlyLosing = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if (bOnlyWinning && bOnlyLosing) {
+    printf("can't specify both -only_winning and -only_losing\n");
+    return 3;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 4;
+  }
+
+  line_no = 0;
+  count = 0;
+  max_streak = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%d",&work);
+
+    if (work < 0)
+      count++;
+    else {
+      if (count > max_streak)
+        max_streak = count;
+
+      count = 0;
+    }
+  }
+
+  fclose(fptr);
+
+  if (!bOnlyWinning || (work > 0)) {
+    if (!bOnlyLosing || (work < 0)) {
+      if (!bDebug)
+        printf("%3d\n",max_streak);
+      else
+        printf("%3d %s\n",max_streak,save_dir);
+    }
+  }
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
