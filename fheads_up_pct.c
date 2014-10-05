47b930647c4c86bca90351836bf42bc97a80568d
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-10-05 11:04:21 -0400

    add date to verbose output

--- fheads_up_pct.c
+++ fheads_up_pct.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
 
 #define MAX_FILENAME_LEN 1024
 static char filename[MAX_FILENAME_LEN];
@@ -15,6 +23,7 @@ static char street_marker[] = "*** ";
 #define STREET_MARKER_LEN (sizeof (street_marker) - 1)
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int get_date_from_cwd(char *cwd,char **date_string_ptr);
 
 int main(int argc,char **argv)
 {
@@ -22,6 +31,8 @@ int main(int argc,char **argv)
   bool bTerse;
   bool bVerbose;
   FILE *fptr0;
+  int retval;
+  char *date_string;
   int filename_len;
   int num_files;
   FILE *fptr;
@@ -59,9 +70,20 @@ int main(int argc,char **argv)
     return 3;
   }
 
+  if (!bTerse) {
+    getcwd(save_dir,_MAX_PATH);
+
+    retval = get_date_from_cwd(save_dir,&date_string);
+
+    if (retval) {
+      printf("get_date_from_cwd() failed: %d\n",retval);
+      return 4;
+    }
+  }
+
   if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
     printf(couldnt_open,argv[curr_arg]);
-    return 4;
+    return 5;
   }
 
   num_files = 0;
@@ -126,7 +148,7 @@ int main(int argc,char **argv)
   if (bTerse)
     printf("%lf\n",heads_up_pct);
   else
-    printf("%lf (%d of %d)\n",heads_up_pct,num_heads_up_hands,num_hands);
+    printf("%lf (%d %d) %s\n",heads_up_pct,num_heads_up_hands,num_hands,date_string);
 
   return 0;
 }
@@ -153,4 +175,42 @@ static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
 
   line[local_line_len] = 0;
   *line_len = local_line_len;
+}
+
+static char sql_date_string[11];
+
+static int get_date_from_cwd(char *cwd,char **date_string_ptr)
+{
+  int n;
+  int len;
+  int slash_count;
+
+  len = strlen(cwd);
+  slash_count = 0;
+
+  for (n = len - 1; (n >= 0); n--) {
+    if (cwd[n] == '/') {
+      slash_count++;
+
+      if (slash_count == 2)
+        break;
+    }
+  }
+
+  if (slash_count != 2)
+    return 1;
+
+  if (cwd[n+5] != '/')
+    return 2;
+
+  strncpy(sql_date_string,&cwd[n+1],4);
+  sql_date_string[4] = '-';
+  strncpy(&sql_date_string[5],&cwd[n+6],2);
+  sql_date_string[7] = '-';
+  strncpy(&sql_date_string[8],&cwd[n+8],2);
+  sql_date_string[10] = 0;
+
+  *date_string_ptr = sql_date_string;
+
+  return 0;
 }
