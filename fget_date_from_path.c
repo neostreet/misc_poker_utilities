e2205f43c4902c27d7ca677babab542fc3c7677b
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-11-04 08:40:31 -0500

    add fget_date_from_path.c

---
+++ fget_date_from_path.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char usage[] =
+"usage: fget_date_from_path slash_char num_slashes filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int get_date_from_path(char *path,char slash_char,int num_slashes,char **date_string_ptr);
+
+int main(int argc,char **argv)
+{
+  int n;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int retval;
+  int num_slashes;
+  char *date_string;
+
+  if (argc != 4) {
+    printf(usage);
+    return 1;
+  }
+
+  sscanf(argv[2],"%d",&num_slashes);
+
+  if ((fptr = fopen(argv[3],"r")) == NULL) {
+    printf(couldnt_open,argv[3]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    retval = get_date_from_path(line,argv[1][0],num_slashes,&date_string);
+
+    if (retval) {
+      printf("get_date_from_path() failed: %d\n",retval);
+      return 3;
+    }
+
+    printf("%s\n",date_string);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static char sql_date_string[11];
+
+static int get_date_from_path(char *path,char slash_char,int num_slashes,char **date_string_ptr)
+{
+  int n;
+  int len;
+  int slash_count;
+
+  len = strlen(path);
+  slash_count = 0;
+
+  for (n = len - 1; (n >= 0); n--) {
+    if (path[n] == slash_char) {
+      slash_count++;
+
+      if (slash_count == num_slashes)
+        break;
+    }
+  }
+
+  if (slash_count != num_slashes)
+    return 1;
+
+  if (path[n+5] != slash_char)
+    return 2;
+
+  strncpy(sql_date_string,&path[n+1],4);
+  sql_date_string[4] = '-';
+  strncpy(&sql_date_string[5],&path[n+6],2);
+  sql_date_string[7] = '-';
+  strncpy(&sql_date_string[8],&path[n+8],2);
+  sql_date_string[10] = 0;
+
+  *date_string_ptr = sql_date_string;
+
+  return 0;
+}
