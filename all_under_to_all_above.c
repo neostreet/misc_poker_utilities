913187ff5cd699697b897de0f9b8f18b38449593
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-11-19 08:24:47 -0500

    add -reverse flag

--- all_under_to_all_above.c
+++ all_under_to_all_above.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_LINE_LEN 8192
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: skeleton filename\n";
+static char usage[] =
+"usage: all_under_to_all_above (-reverse) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  int curr_arg;
+  bool bReverse;
   FILE *fptr;
   int line_len;
   int line_no;
   double pct;
   double save_pct;
 
-  if (argc != 2) {
+  if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
+  bReverse = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-reverse"))
+      bReverse = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
     return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
   }
 
   line_no = 0;
@@ -38,8 +56,16 @@ int main(int argc,char **argv)
 
     sscanf(line,"%lf",&pct);
 
-    if ((line_no > 1) && (save_pct == (double)1) && (pct == (double)0))
-      printf("%s\n",line);
+    if (line_no > 1) {
+      if (!bReverse) {
+        if ((save_pct == (double)1) && (pct == (double)0))
+          printf("%s\n",line);
+      }
+      else {
+        if ((save_pct == (double)0) && (pct == (double)1))
+          printf("%s\n",line);
+      }
+    }
 
     save_pct = pct;
   }
