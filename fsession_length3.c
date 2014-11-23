5156b704865e996be5f1e677fc29041ed1e2ca39
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-11-23 18:09:02 -0500

    add -get_date_from_filename flag

--- fsession_length3.c
+++ fsession_length3.c
@@ -16,7 +16,7 @@ static char line[MAX_LINE_LEN];
 
 static char usage[] =
 "usage: fsession_length3 (-terse) (-debug) (-gelength) (-ltlength)\n"
-"  (-filename_only) player_name filename\n";
+"  (-filename_only) (-get_date_from_filename) player_name filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static char in_chips[] = " in chips";
@@ -27,6 +27,7 @@ static char summary[] = "*** SUMMARY ***";
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 static int Contains(bool bCaseSens,char *line,int line_len,
   char *string,int string_len,int *index);
+static int get_date_from_path(char *path,char slash_char,int num_slashes,char **date_string_ptr);
 
 int main(int argc,char **argv)
 {
@@ -41,6 +42,7 @@ int main(int argc,char **argv)
   bool bLtLength;
   int lt_length;
   bool bFilenameOnly;
+  bool bGetDateFromFilename;
   int player_name_ix;
   int player_name_len;
   FILE *fptr0;
@@ -48,6 +50,8 @@ int main(int argc,char **argv)
   FILE *fptr;
   int line_len;
   int line_no;
+  int retval;
+  char *date_string;
   int dbg_line_no;
   int ix;
   int file_no;
@@ -56,7 +60,7 @@ int main(int argc,char **argv)
   int dbg;
   bool bSkipping;
 
-  if ((argc < 3) || (argc > 8)) {
+  if ((argc < 3) || (argc > 9)) {
     printf(usage);
     return 1;
   }
@@ -66,13 +70,15 @@ int main(int argc,char **argv)
   bGeLength = false;
   bLtLength = false;
   bFilenameOnly = false;
+  bGetDateFromFilename = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-terse"))
       bTerse = true;
     else if (!strcmp(argv[curr_arg],"-debug"))
       bDebug = true;
-    else if (!strncmp(argv[curr_arg],"-ge",3)) {
+    else if (!strncmp(argv[curr_arg],"-ge",3) &&
+      (argv[curr_arg][3] >= '0') && (argv[curr_arg][3] <= '9')) {
       bGeLength = true;
       sscanf(&argv[curr_arg][3],"%d",&ge_length);
     }
@@ -82,6 +88,8 @@ int main(int argc,char **argv)
     }
     else if (!strcmp(argv[curr_arg],"-filename_only"))
       bFilenameOnly = true;
+    else if (!strcmp(argv[curr_arg],"-get_date_from_filename"))
+      bGetDateFromFilename = true;
     else
       break;
   }
@@ -121,6 +129,15 @@ int main(int argc,char **argv)
     if ((fptr = fopen(filename,"r")) == NULL) {
       printf(couldnt_open,filename);
       continue;
+    }
+
+    if (bGetDateFromFilename) {
+      retval = get_date_from_path(filename,'\\',3,&date_string);
+
+      if (retval) {
+        printf("get_date_from_path() on %s failed: %d\n",filename,retval);
+        continue;
+      }
     }
 
     line_no = 0;
@@ -175,8 +192,12 @@ int main(int argc,char **argv)
           printf("%3d\n",num_hands);
         else if (bFilenameOnly)
           printf("%s\n",filename);
-        else
-          printf("%3d %s\n",num_hands,filename);
+        else {
+          if (!bGetDateFromFilename)
+            printf("%3d %s\n",num_hands,filename);
+          else
+            printf("%d\t%s\n",num_hands,date_string);
+        }
       }
     }
   }
@@ -243,4 +264,42 @@ static int Contains(bool bCaseSens,char *line,int line_len,
   }
 
   return false;
+}
+
+static char sql_date_string[11];
+
+static int get_date_from_path(char *path,char slash_char,int num_slashes,char **date_string_ptr)
+{
+  int n;
+  int len;
+  int slash_count;
+
+  len = strlen(path);
+  slash_count = 0;
+
+  for (n = len - 1; (n >= 0); n--) {
+    if (path[n] == slash_char) {
+      slash_count++;
+
+      if (slash_count == num_slashes)
+        break;
+    }
+  }
+
+  if (slash_count != num_slashes)
+    return 1;
+
+  if (path[n+5] != slash_char)
+    return 2;
+
+  strncpy(sql_date_string,&path[n+1],4);
+  sql_date_string[4] = '-';
+  strncpy(&sql_date_string[5],&path[n+6],2);
+  sql_date_string[7] = '-';
+  strncpy(&sql_date_string[8],&path[n+8],2);
+  sql_date_string[10] = 0;
+
+  *date_string_ptr = sql_date_string;
+
+  return 0;
 }
