22e40510d9e07efdf77b4db33f48e39a2751702d
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-04-28 09:01:33 -0400

    add -verbose and -offsetoffset flags

--- run_gold_distance.c
+++ run_gold_distance.c
@@ -1,23 +1,51 @@
 #include <stdio.h>
+#include <string.h>
 
-static char usage[] = "usage: run_gold_distance filename\n";
+static char usage[] =
+"usage: run_gold_distance (-verbose) (-offsetoffset) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  int curr_arg;
+  bool bVerbose;
+  int offset;
   FILE *fptr;
+  int line_len;
   int nobs;
   int wins;
   int losses;
   int work;
 
-  if (argc != 2) {
+  if ((argc < 2) || (argc > 4)) {
     printf(usage);
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
+  bVerbose = false;
+  offset = 0;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else if (!strncmp(argv[curr_arg],"-offset",7))
+      sscanf(&argv[curr_arg][7],"%d",&offset);
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
     return 2;
   }
 
@@ -26,12 +54,14 @@ int main(int argc,char **argv)
   losses = 0;
 
   for ( ; ; ) {
-    fscanf(fptr,"%d",&work);
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
 
     if (feof(fptr))
       break;
 
     nobs++;
+
+    sscanf(&line[offset],"%d",&work);
 
     if (work > 0)
       wins++;
@@ -40,10 +70,37 @@ int main(int argc,char **argv)
 
     work = losses * 2 - wins;
 
-    printf("%3d\n",work);
+    if (!bVerbose)
+      printf("%3d\n",work);
+    else
+      printf("%3d %s\n",work,line);
   }
 
   fclose(fptr);
 
   return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
 }
