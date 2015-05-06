ac3811f06d73f8475caa73a130f41eb3444f69e7
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-05-05 23:33:05 -0400

    add gen_style_and_flavor.c

---
+++ gen_style_and_flavor.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: gen_style_and_flavor filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static char headings[] =
+  "Style Flavor\n";
+
+static char *poker_styles[] = {
+  "Cash ",
+  "SNG  ",
+  "MTT  "
+};
+#define NUM_POKER_STYLES (sizeof poker_styles / sizeof (char *))
+
+static char *poker_flavors[] = {
+  "Pot Limit Hold'em",
+  "Pot Limit Omaha",
+  "Limit 7 Card Stud",
+  "NLHE  ",
+  "Limit 5 Card Draw",
+  "No Limit Triple Draw 2-7 Lowball",
+  "Pot Limit Triple Draw 2-7 Lowball",
+  "Limit Hold'em",
+  "Limit Triple Draw 2-7 Lowball",
+  "8-Game",
+  "HORSE "
+};
+#define NUM_POKER_FLAVORS (sizeof poker_flavors / sizeof (char *))
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int style;
+  int flavor;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  printf("%s\n",headings);
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%d %d",&style,&flavor);
+
+    if (style >= NUM_POKER_STYLES) {
+      printf("invalid style %d on line %d\n",style,line_no);
+      return 3;
+    }
+
+    if (flavor >= NUM_POKER_FLAVORS) {
+      printf("invalid flavor %d on line %d\n",flavor,line_no);
+      return 4;
+    }
+
+    printf("%s %s\n",poker_styles[style],poker_flavors[flavor]);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
