d0f37fe43b3e790dc1f9089e227516e8d94da3d0
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-05-15 08:39:58 -0400

    add gen_twifs2.c

---
+++ gen_twifs2.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static char usage[] = "usage: gen_twifs2 filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+#define BUF_LEN 1024
+static char buf[BUF_LEN];
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int firsts;
+  int seconds;
+  int thirds;
+  int fourths;
+  int fifths;
+  int sixths;
+  int total;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%d %d %d %d %d %d",&firsts,&seconds,&thirds,&fourths,&fifths,&sixths);
+    printf("%d %d %d %d %d %d\n",firsts,seconds,thirds,fourths,fifths,sixths);
+
+    total = firsts + seconds + thirds + fourths + fifths + sixths;
+
+    sprintf(buf,"tournament_wif /cygdrive/c/aidan/pokerstars/50000a %d %d %d",firsts,seconds,total);
+    system(buf);
+    sprintf(buf,"tournament_wif /cygdrive/c/aidan/pokerstars/50000b %d %d %d",firsts,seconds,total);
+    system(buf);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
