d2e96ac0fa4bc7f53f5d162a5fe14a9a55b5a7f3
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-05-25 21:26:07 -0400

    add num_sit_and_gos argument

--- gen_place_counts.c
+++ gen_place_counts.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+static char usage[] = "usage: gen_place_counts num_sit_and_gos\n";
+
 int main(int argc,char **argv)
 {
   int m;
   int n;
+  int num_sit_and_gos;
 
-  for (m = 0; m <= 6; m++) {
-    for (n = 0; n <= 6 - m; n++) {
-      printf("%d %d 6\n",m,n);
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  sscanf(argv[1],"%d",&num_sit_and_gos);
+
+  for (m = 0; m <= num_sit_and_gos; m++) {
+    for (n = 0; n <= num_sit_and_gos - m; n++) {
+      printf("%d %d %d\n",m,n,num_sit_and_gos);
     }
   }
 
