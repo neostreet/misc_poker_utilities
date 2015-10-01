2941b96f95bbd89e7f5924d7178d279ab552e093
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-09-30 20:55:35 -0400

    add -print_ending_balance flag

--- fdelta.c
+++ fdelta.c
@@ -36,7 +36,8 @@ static char usage[] =
 "  (-winning_only) (-losing_only) (-pocket_pairs_only) (-file_names)\n"
 "  (-big_blind) (-8game) (-all_in) (-hand_number) (-ge_valval) (-no_rake)\n"
 "  (-no_hole_cards) (-only_winning_deltas) (-only_losing_deltas)\n"
-"  (-show_collected) (-show_spent) (-show_wagered) (-sum2) player_name filename\n";
+"  (-show_collected) (-show_spent) (-show_wagered) (-sum2)\n"
+"  (-print_ending_balance) player_name filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static char pokerstars[] = "PokerStars";
@@ -132,6 +133,7 @@ int main(int argc,char **argv)
   int show_spent;
   int show_wagered;
   bool bSum2;
+  bool bPrintEndingBalance;
   int player_name_ix;
   int player_name_len;
   int ge_val;
@@ -179,7 +181,7 @@ int main(int argc,char **argv)
   int last_big_blind;
   int rake;
 
-  if ((argc < 3) || (argc > 26)) {
+  if ((argc < 3) || (argc > 27)) {
     printf(usage);
     return 1;
   }
@@ -206,6 +208,7 @@ int main(int argc,char **argv)
   show_spent = 0;
   show_wagered = 0;
   bSum2 = false;
+  bPrintEndingBalance = false;
   ge_val = -1;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
@@ -259,6 +262,8 @@ int main(int argc,char **argv)
       show_wagered = 1;
     else if (!strcmp(argv[curr_arg],"-sum2"))
       bSum2 = true;
+    else if (!strcmp(argv[curr_arg],"-print_ending_balance"))
+      bPrintEndingBalance = true;
     else
       break;
   }
@@ -723,10 +728,18 @@ int main(int argc,char **argv)
         else if (bTerse) {
           if (!bBigBlind) {
             if (!b8game) {
-              if (!bHandNumber)
-                printf("%d\n",quantum);
-              else
-                printf("%d (%d)\n",quantum,num_hands);
+              if (!bHandNumber) {
+                if (!bPrintEndingBalance)
+                  printf("%d\n",quantum);
+                else
+                  printf("%d %d\n",quantum,ending_balance);
+              }
+              else {
+                if (!bPrintEndingBalance)
+                  printf("%d (%d)\n",quantum,num_hands);
+                else
+                  printf("%d %d (%d)\n",quantum,ending_balance,num_hands);
+              }
             }
             else {
               if (!bHandNumber)
