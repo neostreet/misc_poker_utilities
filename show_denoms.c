f0d6413fde8840ba65abc3c212dd288eb96eafce
Aidan Lloyd <lloyd.aidan@gmail.com>
2016-03-12 12:06:08 -0500

    add -missing flag

--- show_denoms.c
+++ show_denoms.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
+static char usage[] = "show_denoms (-missing)\n";
 
 static char *denoms[] = {
  "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"
@@ -18,16 +21,45 @@ int main(int argc,char **argv)
 {
   int m;
   int n;
+  int curr_arg;
+  bool bMissing;
   struct stat statbuf;
+
+  if (argc > 2) {
+    printf(usage);
+    return 1;
+  }
+
+  bMissing = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-missing"))
+      bMissing = true;
+    else
+      break;
+  }
+
+  if (curr_arg != argc) {
+    printf(usage);
+    return 2;
+  }
 
   for (m = 0; m < NUM_COLORS; m++) {
     for (n = 0; n < NUM_DENOMS; n++) {
       sprintf(buf,"%s%s",denoms[n],colors[m]);
 
-      if (!stat(buf,&statbuf))
-        printf("%s",buf);
-      else
-        printf("  ");
+      if (!bMissing) {
+        if (!stat(buf,&statbuf))
+          printf("%s",buf);
+        else
+          printf("  ");
+      }
+      else {
+        if (stat(buf,&statbuf))
+          printf("%s",buf);
+        else
+          printf("  ");
+      }
 
       if (n < NUM_DENOMS - 1)
         putchar(' ');
