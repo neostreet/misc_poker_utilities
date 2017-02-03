95fb4d3b46987c5e206cb01fa3720ee7e2ba6bb5
Aidan Lloyd <lloyd.aidan@gmail.com>
2017-02-02 20:32:11 -0500

    add ftable_chips_equal.c

---
+++ ftable_chips_equal.c
@@ -0,0 +1,204 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: ftable_chips_equal (-verbose) (-first_hand) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static char in_chips[] = " in chips";
+#define IN_CHIPS_LEN (sizeof (in_chips) - 1)
+static char street_marker[] = "*** ";
+#define STREET_MARKER_LEN (sizeof (street_marker) - 1)
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bVerbose;
+  bool bFirstHand;
+  FILE *fptr0;
+  int filenamelen;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int ix;
+  int table_chips_equal_count;
+  int work;
+  int first_player_chips;
+
+  if ((argc < 2) && (argc > 4)) {
+    printf(usage);
+    return 1;
+  }
+
+  getcwd(save_dir,_MAX_PATH);
+
+  bVerbose = false;
+  bFirstHand = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else if (!strcmp(argv[curr_arg],"-first_hand"))
+      bFirstHand = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  table_chips_equal_count = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filenamelen,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    line_no = 0;
+    first_player_chips = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      line_no++;
+
+      if (Contains(true,
+        line,line_len,
+        in_chips,IN_CHIPS_LEN,
+        &ix)) {
+
+        line[ix] = 0;
+
+        for (ix--; (ix >= 0) && (line[ix] != '('); ix--)
+          ;
+
+        sscanf(&line[ix+1],"%d",&work);
+
+        if (!first_player_chips)
+          first_player_chips = work;
+        else if (work != first_player_chips)
+          break;
+      }
+      else if (Contains(true,
+        line,line_len,
+        street_marker,STREET_MARKER_LEN,
+        &ix)) {
+
+        break;
+      }
+    }
+
+    fclose(fptr);
+
+    if (work == first_player_chips) {
+      table_chips_equal_count++;
+
+      if (bVerbose)
+        printf("%s/%s\n",save_dir,filename);
+    }
+
+    if (bFirstHand)
+      break;
+  }
+
+  fclose(fptr0);
+
+  if (!bVerbose)
+    printf("%10d %s\n",table_chips_equal_count,save_dir);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
