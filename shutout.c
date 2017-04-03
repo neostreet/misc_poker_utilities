ceb0224587a0801ca966d6432fd938fba285d5a0
Aidan Lloyd <lloyd.aidan@gmail.com>
2017-04-02 21:30:46 -0400

    add shutout.c

---
+++ shutout.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: shutout filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int delta;
+  bool bShutout;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+  bShutout = true;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%d",&delta);
+
+    if (delta > 0) {
+      bShutout = false;
+      break;
+    }
+  }
+
+  fclose(fptr);
+
+  if (bShutout)
+    printf("%s (%d)\n",argv[1],line_no);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
