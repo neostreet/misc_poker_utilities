06beb0ebb06b51050c902e870b3d38cc4d429d42
Aidan Lloyd <lloyd.aidan@gmail.com>
2019-02-17 16:19:17 -0500

    add fko_delta.c

---
+++ fko_delta.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+#define BUY_IN 50000
+#define BOUNTY 21250
+
+static int ko_payouts[] {
+  95625,
+  57375,
+  38250
+};
+
+static char usage[] = "usage: fko_delta filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int place;
+  int num_bounties;
+  int delta;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+    sscanf(line,"%d %d",&place,&num_bounties);
+
+    if ((place < 0) || (place > 3)) {
+      printf("error on line %d: place must be >= 0 and <= 3\n",line_no);
+      continue;
+    }
+
+    if ((num_bounties < 0) || (num_bounties > 9)) {
+      printf("error on line %d: num_bounties must be >= 0 and <= 9\n",line_no);
+      continue;
+    }
+
+    delta = BUY_IN * -1;
+
+    if (place)
+      delta += ko_payouts[place - 1];
+
+    if (num_bounties)
+      delta += num_bounties * BOUNTY;
+
+    printf("%d\t%d\t%d\n",place,num_bounties,delta);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
