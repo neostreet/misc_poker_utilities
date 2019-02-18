cf40fb0f76195d1184be9db72aae1534827e85f1
Aidan Lloyd <lloyd.aidan@gmail.com>
2019-02-17 21:07:24 -0500

    add -deltal and -place flags

--- ko_payouts.c
+++ ko_payouts.c
@@ -4,23 +4,26 @@
 
 struct ko_payout_in {
   int base_payout;
+  int place;
   int min_bounties;
   int max_bounties;
 };
 
 struct ko_payout_out {
   int base_payout;
+  int place;
   int num_bounties;
   int total_payout;
 };
 
+#define BUY_IN 50000
 #define BOUNTY 21250
 
 struct ko_payout_in ko_payouts_in[] = {
-  {95625, 2, 9},
-  {57375, 0, 7},
-  {38250, 0, 6},
-  {0, 0, 5}
+  {95625, 1, 2, 9},
+  {57375, 2, 0, 7},
+  {38250, 3, 0, 6},
+  {0, 0, 0, 5}
 };
 #define NUM_BASES (sizeof ko_payouts_in / sizeof (struct ko_payout_in))
 #define NUM_PAYOUTS 29
@@ -34,23 +37,41 @@ int main(int argc,char **argv)
   int m;
   int n;
   int p;
+  int curr_arg;
   int payout;
   bool bSort;
+  bool bDelta;
+  bool bPlace;
   int vals[NUM_PAYOUTS];
   int ixs[NUM_PAYOUTS];
 
-  if ((argc == 2) && !strcmp(argv[1],"-sort"))
-    bSort = true;
-  else
-    bSort = false;
+  bSort = false;
+  bDelta = false;
+  bPlace = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-sort"))
+      bSort = true;
+    else if (!strcmp(argv[curr_arg],"-delta"))
+      bDelta = true;
+    else if (!strcmp(argv[curr_arg],"-place"))
+      bPlace = true;
+    else
+      break;
+  }
 
   p = 0;
 
   for (m = 0; m < NUM_BASES; m++) {
     for (n = ko_payouts_in[m].max_bounties; n >= ko_payouts_in[m].min_bounties; n--) {
       ko_payouts_out[p].base_payout = ko_payouts_in[m].base_payout;
+      ko_payouts_out[p].place = ko_payouts_in[m].place;
       ko_payouts_out[p].num_bounties = n;
       ko_payouts_out[p].total_payout = ko_payouts_in[m].base_payout + n * BOUNTY;
+
+      if (bDelta)
+        ko_payouts_out[p].total_payout -= BUY_IN;
+
       p++;
     }
   }
@@ -62,8 +83,10 @@ int main(int argc,char **argv)
     qsort(ixs,NUM_PAYOUTS,sizeof (int),compare);
 
   for (n = 0; n < NUM_PAYOUTS; n++) {
-    printf("%d %d %d\n",ko_payouts_out[ixs[n]].total_payout,
-      ko_payouts_out[ixs[n]].base_payout,ko_payouts_out[ixs[n]].num_bounties);
+    printf("%d %d %d\n",
+      ko_payouts_out[ixs[n]].total_payout,
+      (bPlace ? ko_payouts_out[ixs[n]].place : ko_payouts_out[ixs[n]].base_payout),
+      ko_payouts_out[ixs[n]].num_bounties);
   }
 
   return 0;
