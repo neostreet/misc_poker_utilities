532ed27ddcb3d23d156a01cdeb9494131d59297f
Aidan Lloyd <lloyd.aidan@gmail.com>
2019-03-17 21:32:07 -0400

    add ftable_chip_pct.c

---
+++ ftable_chip_pct.c
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: ftable_chip_pct (-verbose) player_name filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static char in_chips[] = " in chips";
+#define IN_CHIPS_LEN (sizeof (in_chips) - 1)
+static char street_marker[] = "*** ";
+#define STREET_MARKER_LEN (sizeof (street_marker) - 1)
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bVerbose;
+  int player_name_ix;
+  int player_name_len;
+  FILE *fptr0;
+  int filenamelen;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int ix;
+  int table_chips;
+  int work;
+  int player_chips;
+  double dwork;
+
+  if ((argc < 3) || (argc > 4)) {
+    printf(usage);
+    return 1;
+  }
+
+  getcwd(save_dir,_MAX_PATH);
+
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 2) {
+    printf(usage);
+    return 2;
+  }
+
+  player_name_ix = curr_arg++;
+  player_name_len = strlen(argv[player_name_ix]);
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filenamelen,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    line_no = 0;
+    table_chips = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      line_no++;
+
+      if (Contains(true,
+        line,line_len,
+        in_chips,IN_CHIPS_LEN,
+        &ix)) {
+
+        line[ix] = 0;
+
+        for (ix--; (ix >= 0) && (line[ix] != '('); ix--)
+          ;
+
+        sscanf(&line[ix+1],"%d",&work);
+        table_chips += work;
+
+        if (Contains(true,
+          line,line_len,
+          argv[player_name_ix],player_name_len,
+          &ix)) {
+            player_chips = work;
+        }
+      }
+      else if (Contains(true,
+        line,line_len,
+        street_marker,STREET_MARKER_LEN,
+        &ix)) {
+
+        break;
+      }
+    }
+
+    fclose(fptr);
+
+    dwork = (double)player_chips / (double)table_chips;
+
+    if (!bVerbose)
+      printf("%lf %s/%s\n",dwork,save_dir,filename);
+    else
+      printf("%lf (%d %d) %s/%s\n",dwork,player_chips,table_chips,save_dir,filename);
+  }
+
+  fclose(fptr0);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
