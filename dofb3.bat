97d2f22a941cfc89576e3ddfe42d07341fc8ad9c
neostreet <lloyd.aidan@gmail.com>
2023-09-06 17:23:32 -0400

    Add files via upload

---
+++ dofb3.bat
@@ -0,0 +1,4 @@
+@echo off
+call do_format_balances3
+unfixtxt format_balances3.out
+notepad format_balances3.out
---
+++ dofb5.bat
@@ -0,0 +1,4 @@
+@echo off
+call do_format_balances5
+unfixtxt format_balances5.out
+notepad format_balances5.out
