ecf510541064072c7b308099de531e1cdf631296
neostreet <lloyd.aidan@gmail.com>
2025-02-07 00:40:44 -0500

    switch the order of the arguments

--- prem_freq.c
+++ prem_freq.c
@@ -8,13 +8,13 @@ using namespace std;
 #include "poker_hand.h"
 
 static char usage[] =
-"usage: prem_freq total_hands total_premium_hands\n";
+"usage: prem_freq total_premium_hands total_hands\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 int main(int argc,char **argv)
 {
-  int total_hands;
   int total_premium_hands;
+  int total_hands;
   double ratio1;
   double ratio2;
   double dwork;
@@ -24,14 +24,14 @@ int main(int argc,char **argv)
     return 1;
   }
 
-  sscanf(argv[1],"%d",&total_hands);
-  sscanf(argv[2],"%d",&total_premium_hands);
+  sscanf(argv[1],"%d",&total_premium_hands);
+  sscanf(argv[2],"%d",&total_hands);
 
   ratio1 = (double)total_premium_hands / (double)total_hands;
   ratio2 = (double)NUM_PREMIUM_HANDS_IN_DECK / (double)POKER_52_2_PERMUTATIONS;
 
   dwork = ratio1 / ratio2;
-  printf("%lf (%d %d)\n",dwork,total_hands,total_premium_hands);
+  printf("%lf (%d %d)\n",dwork,total_premium_hands,total_hands);
 
   return 0;
 }
